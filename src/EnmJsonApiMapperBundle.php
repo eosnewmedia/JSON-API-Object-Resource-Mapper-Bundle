e886bf254d9dbd01f0e72c907a5d0b5e68f59788
Philipp Marien <marien@eosnewmedia.de>
2018-10-10 15:06:51 +0200

    fixed naming for symfony

---
+++ src/EnmJsonApiMapperBundle.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Enm\Bundle\JsonApi\Mapper;
+
+/**
+ * @author Philipp Marien <marien@eosnewmedia.de>
+ * @deprecated
+ */
+class EnmJsonApiMapperBundle extends EnmJsonApiObjectResourceMapperBundle
+{
+
+}
